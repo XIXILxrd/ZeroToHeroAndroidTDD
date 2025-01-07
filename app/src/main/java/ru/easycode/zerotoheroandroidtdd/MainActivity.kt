65f775fc075f25c61b4b14819e7fefaffa108ecf
xixil <ya.mr-adam@yandex.com>
2025-01-07 16:35:51 +0500

    test

--- app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
+++ app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
@@ -2,10 +2,37 @@ package ru.easycode.zerotoheroandroidtdd
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import ru.easycode.zerotoheroandroidtdd.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.titleTextView.text = savedInstanceState?.getString(TITLE_KEY) ?: getString(R.string.hello_world)
+
+        binding.changeButton.setOnClickListener {
+            binding.titleTextView.text = getString(R.string.i_am_android_developer)
+        }
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+
+        outState.putString(TITLE_KEY, binding.titleTextView.text.toString())
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+
+        binding.titleTextView.text = savedInstanceState.getString(TITLE_KEY)
+    }
+
+    companion object {
+        private const val TITLE_KEY = "TITLE_KEY"
     }
 }
