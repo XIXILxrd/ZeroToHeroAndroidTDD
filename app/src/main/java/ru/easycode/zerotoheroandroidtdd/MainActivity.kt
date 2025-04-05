99400c440e436df10da07b7f1d4a35c27d4671e1
rukhlov_ar <ruhlovar.dev@yandex.com>
2025-04-05 19:42:14 +0500

    solved

--- app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
+++ app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
@@ -2,10 +2,42 @@ package ru.easycode.zerotoheroandroidtdd
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import ru.easycode.zerotoheroandroidtdd.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private val binding: ActivityMainBinding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+
+        if (IS_REMOVED) {
+            binding.rootLayout.removeView(binding.titleTextView)
+        }
+
+        binding.removeButton.setOnClickListener {
+            IS_REMOVED = true
+            binding.rootLayout.removeView(binding.titleTextView)
+        }
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+
+        outState.putBoolean(REMOVED_VIEW_KEY, IS_REMOVED)
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+
+        IS_REMOVED = savedInstanceState.getBoolean(REMOVED_VIEW_KEY, false)
+    }
+
+    companion object {
+        private const val REMOVED_VIEW_KEY = "KEY"
+        private var IS_REMOVED: Boolean = false
     }
 }
