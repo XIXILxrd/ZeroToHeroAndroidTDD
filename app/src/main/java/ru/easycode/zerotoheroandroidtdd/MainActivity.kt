8f833cf5963cc97cdfe7d2952030dea179e33b3c
rukhlov_ar <ruhlovar.dev@yandex.com>
2025-04-07 22:44:47 +0500

    solved

--- app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
+++ app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
@@ -1,11 +1,56 @@
 package ru.easycode.zerotoheroandroidtdd
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.ProgressBar
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import ru.easycode.zerotoheroandroidtdd.databinding.ActivityMainBinding
+import java.io.Serializable
 
 class MainActivity : AppCompatActivity() {
+
+    private val binding: ActivityMainBinding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+
+    private var state: State = State.Base()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+
+        binding.actionButton.setOnClickListener {
+            state = State.Loading()
+            state.apply(binding.progressBar, binding.titleTextView, binding.actionButton)
+
+            it.postDelayed({
+                state = State.Base()
+                state.apply(binding.progressBar, binding.titleTextView, binding.actionButton)
+            }, 3_000)
+        }
+    }
+}
+
+interface State : Serializable {
+
+    fun apply(progressBar: ProgressBar, textView: TextView, button: Button)
+
+    class Base() : State {
+
+        override fun apply(progressBar: ProgressBar, textView: TextView, button: Button) {
+            progressBar.visibility = View.GONE
+            textView.visibility = View.VISIBLE
+            button.isEnabled = true
+        }
+    }
+
+    class Loading() : State {
+        override fun apply(progressBar: ProgressBar, textView: TextView, button: Button) {
+            progressBar.visibility = View.VISIBLE
+            textView.visibility = View.GONE
+            button.isEnabled = false
+        }
     }
 }
